64e1470fba80de6d21ad1c521a307bb6f4bc367c
janux <ruojautuma@gmail.com>
2015-03-11 19:18:40 +0200

    fix syntax error

--- modules/heartbeat.js
+++ modules/heartbeat.js
@@ -46,7 +46,7 @@ exports.getLatencies = function(req, res) {
     crypt.sendCryptJSON(latencies, res);
 }
 
-exports.sendHeartBeatRequest(host, callback) {
+exports.sendHeartBeatRequest = function(host, callback) {
     var time = monument.utc().valueOf();
     crypt.encryptJSON({ ping:Globals.uuid, timestamp:time }, function(data) {
         request.post(
