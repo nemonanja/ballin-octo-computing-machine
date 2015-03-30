5e658f3f1424877f75e25e66fddfcdf4a448b0f7
Nemanja <nvukota@student.oulu.fi>
2015-03-30 21:44:53 +0300

    more fix

--- public/app.js
+++ public/app.js
@@ -1,12 +1,6 @@
 $(function() {
     markerIndex = 0;
 
-    /*var markerArray = [
-        {name:'Houston', latLng:[29.761993,-95.369568]},
-        {name:'New York', latLng:[40.710833,-74.002533]},
-        {name:'Kansas City', latLng:[39.115145,-94.633484]}
-    ];*/
-
     $('#world-map').vectorMap({
         markers: []
     });
@@ -19,22 +13,11 @@ $(function() {
     $('#svgMapOverlay').width(width);
     $('#svgMapOverlay').height(height);
 
-    /*console.log(width);
-    console.log(height);
-    console.log($('#svgMapOverlay').width());
-    console.log($('#svgMapOverlay').height());*/
 
     var draw = SVG('svgMapOverlay').size($('#svgMapOverlay').width(),$('#svgMapOverlay').height());
-    //var coords1 = map.latLngToPoint(markerArray[0].latLng[0],markerArray[0].latLng[1]);
-    //var coords2 = map.latLngToPoint(markerArray[1].latLng[0],markerArray[1].latLng[1]);
-    /*console.log(coords1);
-    draw
-        .path()
-        .attr({ fill: 'none',stroke: '#c00', 'stroke-width': 2 })
-        .M(coords1.x, coords1.y)
-        .L(coords2.x, coords2.y);*/
 
     $("#btnTrace").click(function(){
+        removeAllMarkers();
         var ip = document.getElementById("urlip").value;
          $.ajax({
             url: '/gettraceroute',
@@ -47,7 +30,8 @@ $(function() {
                     for(var j=0; j<data[i].traceroute.length; j++) {
                         if(data[i].traceroute[j].geodata.latitude && data[i].traceroute[j].geodata.longitude) {
                             //Check when tracerout ends
-                            if (map.latLngToPoint(data[i].traceroute[j+1].geodata.latitude == null)){
+                            console.log(map.latLngToPoint(data[i].traceroute[j+1].geodata.latitude);
+                            if (map.latLngToPoint(data[i].traceroute[j+1].geodata.latitude == 'undefined')){
                                 break;
                             }
                             //console.log(data[i].traceroute[j].geodata);
