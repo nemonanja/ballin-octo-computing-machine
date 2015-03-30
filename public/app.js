a728577c25f0a9cabf4fe91dfb37a87df922541d
Nemanja <nvukota@student.oulu.fi>
2015-03-30 18:33:22 +0300

    ass

--- public/app.js
+++ public/app.js
@@ -1,14 +1,14 @@
 $(function() {
     markerIndex = 0;
 
-    /*var markerArray = [
+    var markerArray = [
         {name:'Houston', latLng:[29.761993,-95.369568]},
         {name:'New York', latLng:[40.710833,-74.002533]},
         {name:'Kansas City', latLng:[39.115145,-94.633484]}
-    ];*/
+    ];
 
     $('#world-map').vectorMap({
-        markers: []
+        markers: markerArray
     });
     var map = $('#world-map').vectorMap('get', 'mapObject');
 
@@ -25,15 +25,16 @@ $(function() {
     console.log($('#svgMapOverlay').height());*/
 
     var draw = SVG('svgMapOverlay').size($('#svgMapOverlay').width(),$('#svgMapOverlay').height());
-    var coords1 = map.latLngToPoint(markerArray[0].latLng[0],markerArray[0].latLng[1]);
-    var coords2 = map.latLngToPoint(markerArray[1].latLng[0],markerArray[1].latLng[1]);
+    //var coords1 = map.latLngToPoint(markerArray[0].latLng[0],markerArray[0].latLng[1]);
+    //var coords2 = map.latLngToPoint(markerArray[1].latLng[0],markerArray[1].latLng[1]);
+    /*console.log(coords1);
     draw
         .path()
         .attr({ fill: 'none',stroke: '#c00', 'stroke-width': 2 })
         .M(coords1.x, coords1.y)
-        .L(coords2.x, coords2.y);
+        .L(coords2.x, coords2.y);*/
 
-    /*$("#btnTrace").click(function(){
+    $("#btnTrace").click(function(){
         var ip = document.getElementById("urlip").value;
          $.ajax({
             url: '/gettraceroute',
@@ -48,26 +49,23 @@ $(function() {
                             console.log(data[i].traceroute[j].geodata);
                             markerIndex +=1;
                             map.addMarker(markerIndex, [data[i].traceroute[j].geodata.latitude, data[i].traceroute[j].geodata.longitude]);
+                            var coords1 = map.latLngToPoint(data[i].traceroute[j].geodata.latitude,data[i].traceroute[j].geodata.longitude);
+                            var coords2 = map.latLngToPoint(data[i].traceroute[j+1].geodata.latitude,data[i].traceroute[j+1].geodata.longitude);
+                            draw
+                                .path()
+                                .attr({ fill: 'none',stroke: '#c00', 'stroke-width': 2 })
+                                .M(coords1.x, coords1.y)
+                                .L(coords2.x, coords2.y);
                         }
                     }
                 }
             }
         });
 
-    });*/
+    });
 
 });
 
-
-    /*var nodeArray = [{coords: [60, 60], name: 'test2'},{coords: [0, 0], name: 'test3'},{coords: [30, 30], name: 'test4'},{coords: [15, 15], name: 'test5'}];
-
-    
-    for (i=0; i<nodeArray.length; i++){
-        map.addMarker(markerIndex, nodeArray[i]['coords']);
-        console.log(nodeArray[i]['coords']);
-        console.log(markerIndex);
-        markerIndex +=1;
-    }*/
 
 
 
