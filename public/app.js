4805bf2c590a837faea9ebabada53a7bbc49c0d5
Nemanja <nvukota@student.oulu.fi>
2015-03-30 19:09:07 +0300

    more fix

--- public/app.js
+++ public/app.js
@@ -46,16 +46,20 @@ $(function() {
                 for(var i=0; i<data.length; i++) {
                     for(var j=0; j<data[i].traceroute.length; j++) {
                         if(data[i].traceroute[j].geodata.latitude && data[i].traceroute[j].geodata.longitude) {
-                            console.log(data[i].traceroute[j].geodata);
+                            if (map.latLngToPoint(data[i].traceroute[j+1].geodata.latitude == null)){
+                                break;
+                            }
+                            //console.log(data[i].traceroute[j].geodata);
                             markerIndex +=1;
                             map.addMarker(markerIndex, [data[i].traceroute[j].geodata.latitude, data[i].traceroute[j].geodata.longitude]);
                             var coords1 = map.latLngToPoint(data[i].traceroute[j].geodata.latitude,data[i].traceroute[j].geodata.longitude);
                             var coords2 = map.latLngToPoint(data[i].traceroute[j+1].geodata.latitude,data[i].traceroute[j+1].geodata.longitude);
                             draw
                                 .path()
-                                .attr({ fill: 'none',stroke: '#c00', 'stroke-width': 2 })
+                                .attr({ fill: 'none',stroke: '#f213c7', 'stroke-width': 2 })
                                 .M(coords1.x, coords1.y)
                                 .L(coords2.x, coords2.y);
+
                         }
                     }
                 }
